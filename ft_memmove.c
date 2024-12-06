9319281cb22942c04bc29e31bff9184ba9b373c4
Michal Zapora <mzapora@c2r4s4.42warsaw.pl>
2024-12-06 22:24:33 +0100

    libft

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mzapora <mzapora@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 18:33:20 by mzapora           #+#    #+#             */
-/*   Updated: 2024/12/06 19:20:10 by mzapora          ###   ########.fr       */
+/*   Updated: 2024/12/06 22:07:51 by mzapora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,18 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	s = (const unsigned char *)src;
 	d = (unsigned char *)dest;
 	i = 0;
-	if (d < s)
+	if (d > s)
 	{
-		while (n > 0)
-		{
+		while (n--)
 			((char *)d)[n] = ((char *)s)[n];
-			n--;
-		}
 	}
-	if (s < d)
+	else
 	{
 		while (i < n)
 		{
-			((char *)d)[n] = ((char *)s)[n];
+			((char *)d)[i] = ((char *)s)[i];
 			i++;
 		}
 	}
-	return ((char *)d);
+	return ((char *)dest);
 }
